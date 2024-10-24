34fb37785083eaaa9192e33b013bc2710a69acbc
PedroH <141150896+SantosPedroHenrique@users.noreply.github.com>
2024-10-24 11:16:17 -0300

    a

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
 criaCartão(
-'programação',
-'O que é HTML?',
-'É uma linguagem de marcação de hipertexto',
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
 )
 criaCartão(
     'programação',
     'O que é HTML?',
     'É uma linguagem de marcação de hipertexto',
-    )
-    criaCartão(
-        'programação',
-        'O que é HTML?',
-        'É uma linguagem de marcação de hipertexto',
-        )
-        criaCartão(
-            'programação',
-            'O que é HTML?',
-            'É uma linguagem de marcação de hipertexto',
-            )
-            criaCartão(
-                'programação',
-                'O que é HTML?',
-                'É uma linguagem de marcação de hipertexto',
-                )
-                criaCartão(
-                    'programação',
-                    'O que é HTML?',
-                    'É uma linguagem de marcação de hipertexto',
-                    )
+)
+criaCartão(
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
+)
+criaCartão(
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
+)
+criaCartão(
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
+)
+criaCartão(
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
+)
