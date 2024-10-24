6014ba7262d7c4ac9b65dc6f565af080c616856b
PedroH <141150896+SantosPedroHenrique@users.noreply.github.com>
2024-10-24 11:15:00 -0300

    u5

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,28 @@ criaCartão(
 'O que é HTML?',
 'É uma linguagem de marcação de hipertexto',
 )
+criaCartão(
+    'programação',
+    'O que é HTML?',
+    'É uma linguagem de marcação de hipertexto',
+    )
+    criaCartão(
+        'programação',
+        'O que é HTML?',
+        'É uma linguagem de marcação de hipertexto',
+        )
+        criaCartão(
+            'programação',
+            'O que é HTML?',
+            'É uma linguagem de marcação de hipertexto',
+            )
+            criaCartão(
+                'programação',
+                'O que é HTML?',
+                'É uma linguagem de marcação de hipertexto',
+                )
+                criaCartão(
+                    'programação',
+                    'O que é HTML?',
+                    'É uma linguagem de marcação de hipertexto',
+                    )
