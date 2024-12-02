f67384f6f2fbccfb00d3faaa96c85093b4ced758
PedroH <141150896+SantosPedroHenrique@users.noreply.github.com>
2024-12-02 15:54:07 -0300

    alterações

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,71 @@
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
+
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Esportes',
+    'Qual é será o melhor time da série B de 2025?',
+    'Clube do Remo de Belém do Pará'
 )
+
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
+
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
+
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Programação',
+    'O que é um seletor root?',
+    'Um seletor onde podemos criar variáveis no CSS'
 )
+
 criaCartao(
-    'programação',
-    'O que é HTML?',
-    'É uma linguagem de marcação de hipertexto',
+    'Ciências',
+    'Qual a velocidade da luz?',
+    '300 MIL KM por Segundo'
+)
+
+criaCartao(
+    'Física',
+    'Qual a aceleração da gravidade?',
+    'Aproximadamente 9,8m/s'
+)
+
+criaCartao(
+    'Programação',
+    'Qual o nome do bloco do Scratch que verifica condições?',
+    'SE / ENTÃO / SENÃO'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz a frase: "dia do pagamento" ?',
+    'Pay day'
+)
+
+criaCartao(
+    'Lingua Xucreza',
+    'Como se diz "Black Friday" em Xucrês ?',
+    'Blaquifridái'
+)
+
+criaCartao(
+    'Lingua Espanhola',
+    'Como se diz "menino" em Espanhol ?',
+    'Tico'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica a Ilha do Marajó  ?',
+    'localizado no estado do Pará'
 )
